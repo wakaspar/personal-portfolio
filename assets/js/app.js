3853a5d81ee6d76e68dc505ab983f01f6bca7ffc
Will Kaspar <Will@Aang.local>
2017-01-24 17:52:43 -0800

    1/24/17.  Rebuilt CSS, added Indigo-pic, removed redundant <div.spacer> class+elements.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,22 @@
 // sanity check
 console.log("app.js linked.");
+
+
+
+/*var hiddenBox = document.getElementById(hidden-box);
+
+var revealBox = function(){
+    hiddenBox.className = 'not-so-hidden';
+    document.getElementsByClassName('not-so-hidden').removeAttribute('id');
+}
+
+document.getElementById('hidden-box').addEventListener('click', revealBox);
+*/
+
+
+/*var indigoPic = document.getElementById('indigo-pic');
+var someEvent = function(){
+  indigoPic.setAttribute('opacity', 0);
+}
+indigoPic.addEventListener('click', someEvent);
+*/
